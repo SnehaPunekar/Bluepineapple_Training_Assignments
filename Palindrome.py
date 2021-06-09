5b2956653ef358c570e0aa059fb6330f76ec5b37
SnehaPunekar <47572616+SnehaPunekar@users.noreply.github.com>
2021-06-09 22:32:50 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,13 @@
+
+def is_Palindrome(word):
+	rev = word[::-1]
+	print("Reverse is : " + rev)
+	if(rev == word):
+		return 1
+	return 0
+
+word = input("Enter the word to check if it is palindrome or not\n")
+if(is_Palindrome(word) == 1):
+	print (word + " is Palindrome")
+else:
+	print(word + " is not Palindrome")
