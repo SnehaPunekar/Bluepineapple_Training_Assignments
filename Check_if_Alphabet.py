2b2b2acb7be5981667ccd1429983611238c3bc80
SnehaPunekar <47572616+SnehaPunekar@users.noreply.github.com>
2021-07-04 23:43:22 +0530

    Delete Check_if_Alphabet.py

--- Check_if_Alphabet.py
+++
@@ -1,8 +0,0 @@
-# Write a program to check if given character is alphabet or not. */
-
-ch=input("Enter the character:\n")
-
-if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-	print(ch, "is an Alphabet.")
-else:
-	print(ch, "is not an Alphabet.")
